7906dbce4b5a6918cb2689872128ce0bae4ab458
juvemar <martoo.atanasov@gmail.com>
2016-02-14 22:55:42 +0200

    PetsList view model added.

---
+++ PetCare/PetCare.Web/Models/Pet/ListPetsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using PetCare.Web.Infrastructure.Mapping;
+
+namespace PetCare.Web.Models.Pet
+{
+    public class ListPetsViewModel : IMapFrom<PetCare.Models.Pet>
+    {
+        public string Name { get; set; }
+    }
+}
