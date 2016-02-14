9240f0369c9904d1a75808999bb8fc5144d429ba
juvemar <martoo.atanasov@gmail.com>
2016-02-14 19:57:57 +0200

    VetVisitsServices added.

---
+++ PetCare/PetCare.Services/Contracts/IVetVisitsService.cs
@@ -0,0 +1,17 @@
+﻿namespace PetCare.Services.Contracts
+{
+    using System.Linq;
+
+    using Models;
+
+    public interface IVetVisitsService
+    {
+        IQueryable<VetVisit> GetAll();
+
+        IQueryable<VetVisit> GetById(int id);
+
+        void Add(VetVisit vetVisit);
+
+        void UpdatePet(int id);
+    }
+}
---
+++ PetCare/PetCare.Services/VetVisitsService.cs
@@ -0,0 +1,43 @@
+﻿namespace PetCare.Services
+{
+    using System.Linq;
+
+    using PetCare.Data.Repositories;
+    using PetCare.Models;
+    using PetCare.Services.Contracts;
+
+    public class VetVisitsService : IVetVisitsService
+    {
+        private IRepository<VetVisit> vetVisits;
+
+        public VetVisitsService(IRepository<VetVisit> vetVisits)
+        {
+            this.vetVisits = vetVisits;
+        }
+
+        public void Add(VetVisit vetVisit)
+        {
+            this.vetVisits.Add(vetVisit);
+            this.vetVisits.SaveChanges();
+        }
+
+        public IQueryable<VetVisit> GetAll()
+        {
+            return this.vetVisits.All();
+        }
+
+        public IQueryable<VetVisit> GetById(int id)
+        {
+            return this.vetVisits.All().Where(x => x.Id == id).AsQueryable();
+        }
+
+        public void UpdatePet(int id)
+        {
+            var currentPet = this.vetVisits.GetById(id);
+            currentPet.HealthRecordId = id;
+
+            this.vetVisits.Update(currentPet);
+            this.vetVisits.SaveChanges();
+        }
+    }
+}
